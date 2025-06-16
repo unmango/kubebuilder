07e7be8617248fbb378ce7091fc63bc01fda262b
Erik Rasmussen <erik.rasmussen@unmango.dev>
2025-06-15 22:45:01 -0500

    unmango bundle

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -2,8 +2,11 @@ package cli
 
 import (
 	kindv1alpha "github.com/unmango/kubebuilder/plugins/kind/v1alpha"
+	"github.com/unmango/kubebuilder/plugins/unmango"
 	"github.com/unmango/kubebuilder/upstream"
 	"sigs.k8s.io/kubebuilder/v4/pkg/cli"
+	"sigs.k8s.io/kubebuilder/v4/pkg/model/stage"
+	"sigs.k8s.io/kubebuilder/v4/pkg/plugin"
 )
 
 func New() (*cli.CLI, error) {
@@ -12,8 +15,25 @@ func New() (*cli.CLI, error) {
 		return nil, err
 	}
 
+	gov4Bundle, err := upstream.GoV4Bundle()
+	if err != nil {
+		return nil, err
+	}
+
+	unmangov1alphaBundle, err := plugin.NewBundleWithOptions(
+		plugin.WithName(unmango.DefaultNameQualifier),
+		plugin.WithVersion(plugin.Version{Number: 1, Stage: stage.Alpha}),
+		plugin.WithPlugins(gov4Bundle, &kindv1alpha.Plugin{}),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	return cli.New(
 		coreOptions,
-		cli.WithPlugins(&kindv1alpha.Plugin{}),
+		cli.WithPlugins(
+			&kindv1alpha.Plugin{},
+			unmangov1alphaBundle,
+		),
 	)
 }
---
+++ plugins/unmango/unmango.go
@@ -0,0 +1,5 @@
+package unmango
+
+import "sigs.k8s.io/kubebuilder/v4/pkg/plugins"
+
+const DefaultNameQualifier = "unmango." + plugins.DefaultNameQualifier
